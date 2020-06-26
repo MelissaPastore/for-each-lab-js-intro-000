78007851624644687bdb66fc937737f520a42483
Melissa Pastore <melissa.pastore@gmail.com>
2020-06-26 18:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function iterativeLog (array) {
   array.forEach(element, index => console.log(`${index}: ${element}`) )
 }
+
+function doToArray (array, callback) {
+  
+  array.forEach(callback)
+}
